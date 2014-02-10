164337038f4024378e0f476a90b22a8674111d69
ppebaf <robotpicbasic@gmail.com>
2014-02-10 15:17:56 +0100

    MAJ

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php include 'includes/header.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+		<title>GSB</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link href="css/style.css" rel="stylesheet">
+ 
+</head>
+<body>
+
+<header>
+	<img id="logo" src="images/gsb.png" alt="logo" style="width:150px;" />
+	<nav>
+	<ul>
+	<li>Accueil</li>
+	<li>Contact</li>
+	<li>Connexion</li>
+	<li>News</li>
+	</ul>
+	
+	</nav>
+
+
+</header>
+
+
+<div id="container">
+<div id="connect">
+
+<h1>Se connecter :</h1>
+
+<FORM Method="POST" Action="index.php">
+
+Utilisateur :   <INPUT type=text size=20 name=account><BR>
+
+Mot de passe :  <INPUT type=text size=20 name=password><BR>
+
+
+  <INPUT type=submit value=Envoyer>
+
+  <INPUT type=hidden name=afficher value=ok>
+
+</FORM>
+
+</div>
+
+
+
+
+jkdsnj dshgjsd gjdhg hjdb godnvkjd fkgjndfkj fdkjgfdjkg dfkjg dfkj gdfjkbg jdfbg jdfbgjdfsbgjdfbsjgbdfjhbgdfjbgjhdfbgjhdfbgj hbdfj hfj    begjbdf j dfjh dfjhb dfjhb jdhfb gjhbgjdhbf gjdfhb sgjdhb gjhdbgj bfdjhb gjhdbf gljd<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+jkdsnj dshgjsd gjdhg hjdb god<br>
+</div>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+<?php include 'includes/footer.php'; ?>
